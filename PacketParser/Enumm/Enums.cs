60b8704fe16384a621245027037dad1d0d42fea5
AppleSoft <AppleSoft@DESKTOP-32SUNQC>
2020-04-18 18:34:22 +0430

    Sync

---
+++ PacketParser/Enumm/Enums.cs
@@ -0,0 +1,14 @@
+﻿using PacketParser.Services;
+
+namespace PacketParser.Enumm
+{
+    public class Enums
+    {
+        public enum EnPartDate
+        {
+            [PersianNameAttribute.PersianName("سال")] Year = 1,
+            [PersianNameAttribute.PersianName("ماه")] Mounth = 2,
+            [PersianNameAttribute.PersianName("روز")] Day = 3
+        }
+    }
+}
