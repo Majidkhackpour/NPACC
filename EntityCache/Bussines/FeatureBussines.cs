21d816b1c64a6d40d9e94a2b857dcfb7d9f01b50
arad_enj <arad_enj@yahoo.com>
2020-06-10 17:05:22 +0430

    sync

--- EntityCache/Bussines/FeatureBussines.cs
+++ EntityCache/Bussines/FeatureBussines.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using EntityCache.Assistence;
 using Nito.AsyncEx;
@@ -11,7 +12,7 @@ namespace EntityCache.Bussines
     public class FeatureBussines : IFeatures
     {
         public Guid Guid { get; set; }
-        public DateTime Modified { get; set; }
+        public DateTime Modified { get; set; } = DateTime.Now;
         public string Title { get; set; }
 
 
@@ -81,6 +82,38 @@ namespace EntityCache.Bussines
             }
 
             return res;
+        }
+
+        public static async Task<List<FeatureBussines>> GetAllAsync(string search)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(search))
+                    search = "";
+                List<FeatureBussines> res = null;
+                res = await GetAllAsync();
+                var searchItems = search.SplitString();
+                if (searchItems?.Count > 0)
+                    foreach (var item in searchItems)
+                    {
+                        if (!string.IsNullOrEmpty(item) && item.Trim() != "")
+                        {
+                            res = res.Where(x =>
+                                    x.Title.Contains(item))
+                                ?.ToList();
+                        }
+                    }
+
+                res = res?.OrderBy(o => o.Title).ToList();
+                return res;
+            }
+            catch (OperationCanceledException)
+            { return null; }
+            catch (Exception ex)
+            {
+                WebErrorLog.ErrorInstence.StartErrorLog(ex);
+                return new List<FeatureBussines>();
+            }
         }
     }
 }
